1db97f3f89042c3d359ad379af2db63d7ae0a278
muhmp <defcoolz@gmail.com>
2022-06-19 02:18:07 +0900

    updates

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	//Rest Controller
+	//import GetMapping
+	@GetMapping
+/*	public String hello(){
+		return "Hello World";
+	}*/
+	//example list
+	public List<String> hello1(){
+		return List.of("hello", "world");
 	}
 
 }
