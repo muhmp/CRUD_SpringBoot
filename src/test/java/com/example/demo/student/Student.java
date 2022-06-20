be5bf464d2ce2aeffd6772d9b20e1a72854288b4
muhmp <defcoolz@gmail.com>
2022-06-21 07:46:55 +0900

    updates

---
+++ src/test/java/com/example/demo/student/Student.java
@@ -0,0 +1,18 @@
+package com.example.demo.student;
+
+//LocalDate package
+import java.time.LocalDate;
+
+public class Student {
+
+    //id , name, age,
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dob;
+    private Integer Age;
+
+    public Student() {
+    }
+}
