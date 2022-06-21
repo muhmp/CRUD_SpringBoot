a959c01006fd075154c7b8d37464cbc022657771
muhmp <defcoolz@gmail.com>
2022-06-21 21:42:15 +0900

    updates

--- src/test/java/com/example/demo/student/Student.java
+++ src/test/java/com/example/demo/student/Student.java
@@ -11,8 +11,64 @@ public class Student {
     private String email;
     private Integer age;
     private LocalDate dob;
-    private Integer Age;
 
     public Student() {
+    }
+
+    //constructor including the value
+    public Student(Long id, String name, String email, Integer age, LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    //without id
+    public Student(String name, String email, Integer age, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
     }
 }
