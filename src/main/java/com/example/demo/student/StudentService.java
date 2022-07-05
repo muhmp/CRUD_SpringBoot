a1ffba15928e513de39b34dcc3e257e36ce5791d
muhmp <defcoolz@gmail.com>
2022-07-05 22:38:56 +0900

    updates

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,4 +1,23 @@
 package com.example.demo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 public class StudentService {
+    // List of student based on constructor from Student.java
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L, //represent id
+                        "mamet", //represent name
+                        "mamet@gmail.com",//represent email
+                        21,//represent age
+                        LocalDate.of(2000, Month.NOVEMBER,5) //represent dob
+                )
+        );
+    }
 }
